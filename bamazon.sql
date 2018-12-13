6bb8f104446709b159eface29136ed8590e4a399
John Doe <patrick.gould94@gmail.com>
2018-12-13 03:07:29 -0500

    completed customer bamazon

---
+++ bamazon.sql
@@ -0,0 +1,43 @@
+create database bamazon;
+
+create table bamazon.products(
+	id integer not null auto_increment primary key,
+    product_name varchar(50) not null,
+    department_name varchar(50) not null,
+    price int not null,
+    stock_quantity int not null
+);
+
+insert into bamazon.products (product_name, department_name, price, stock_quantity) 
+values ('Logitech G502 Mouse', 'Computer Accessories', '50', '85');
+
+insert into bamazon.products (product_name, department_name, price, stock_quantity) 
+values ('Logitech G402 Mouse', 'Computer Accessories', '40', '100');
+
+insert into bamazon.products (product_name, department_name, price, stock_quantity) 
+values ('Nvidia GTX 1080Ti', 'Computer Hardware', '850', '50');
+
+insert into bamazon.products (product_name, department_name, price, stock_quantity) 
+values ('Nvidia GTX 2080Ti', 'Computer Hardware', '1450', '20');
+
+insert into bamazon.products (product_name, department_name, price, stock_quantity) 
+values ('Intel Core i7-7700K', 'Computer Hardware', '350', '200');
+
+insert into bamazon.products (product_name, department_name, price, stock_quantity) 
+values ('Black Desert Online', 'Video Games', '10', '120');
+
+insert into bamazon.products (product_name, department_name, price, stock_quantity) 
+values ('Hunt Showdown', 'Video Games', '20', '72');
+
+insert into bamazon.products (product_name, department_name, price, stock_quantity) 
+values ('Logitech K840 Keyboard', 'Computer Accessories', '45', '170');
+
+insert into bamazon.products (product_name, department_name, price, stock_quantity) 
+values ('Cooler Master Hyper 212E Fan', 'Computer Hardware', '30', '121');
+
+insert into bamazon.products (product_name, department_name, price, stock_quantity) 
+values ('MSI Gaming Intel B250 Motherboard', 'Computer Hardware','120', '135');
+
+insert into bamazon.products (product_name, department_name, price, stock_quantity) 
+values ('Capri Sun Fruit Punch 10 Pack', 'Food & Drinks', '15', '56');
+
---
+++ bamazonCustomer.js
@@ -0,0 +1,115 @@
+const mysql = require('mysql');
+
+const inquirer = require('inquirer');
+
+const Table = require('cli-table');
+
+const connection = mysql.createConnection({
+    host: "localhost",
+  
+    // Your port; if not 3306
+    port: 3306,
+  
+    // Your username
+    user: "root",
+  
+    // Your password
+    password: "Powerade.1",
+    database: "bamazon"
+});
+
+stock = 0;
+
+connection.connect(function(err) {
+    if (err) throw err;
+});
+
+function showInventory() {
+    var table = new Table({
+        head: ['ID', 'Product', 'Department', 'Price', 'Stock']
+      , colWidths: [5, 50, 35, 9, 8]
+    });
+    
+    connection.query('SELECT * FROM bamazon.products',  function(err,res) {
+        if (err) throw err;
+        
+        for (var i = 0; i < res.length; i++) {
+            let id = res[i].id;
+            let product = res[i].product_name;
+            let department = res[i].department_name;
+            let price = res[i].price;
+            let stock = res[i].stock_quantity;
+
+            table.push(
+                [id, product, department, `$${price}`, stock]
+            );
+
+        }
+        console.log('\n\n' + table.toString());
+    });
+}
+
+function updateQuantity(id, amount) {
+    connection.query(
+        `UPDATE bamazon.products SET stock_quantity = stock_quantity - ${amount} WHERE ?`,
+        [
+        {
+            id: id
+        }],
+        function(err,res) {
+        if (err) throw err;
+        
+        })
+}
+
+function purchaseProduct(id, amount) {
+    connection.query('SELECT product_name, price, stock_quantity FROM bamazon.products WHERE id=?', [id], function(err,res) {
+        if (err) throw err;
+        
+        if (amount <= res[0].stock_quantity) {
+            
+            updateQuantity(id, amount);
+            console.log('\n\n Your purchase was successful.');
+            let purchase = new Table({
+                head: ['Product', 'Price', 'Quantity', 'Total']
+              , colWidths: [40, 9, 12, 10]
+            });
+            let product = res[0].product_name;
+            let price = res[0].price;
+            let total = amount * price;
+            purchase.push(
+                [product, `$${price}`, amount, `$${total}`]
+            );
+            console.log('\n  Invoice:\n' + purchase.toString());
+        }
+
+        else {
+            console.log("\nSorry we are unable to process that purchase. Try ordering a lower quantity.")
+        }
+    });
+    
+    
+}
+
+
+showInventory();
+inquirer.prompt([
+    
+    {
+        type: 'input',
+        name: 'select',
+        message: 'What product would you like to purchase? (Enter product ID number)'
+    },
+    {
+        type: 'input',
+        name: 'amount',
+        message: 'How many units of this product would you like to purchase?'
+    }
+  
+]) 
+.then(function(answers) {
+    purchaseProduct(answers.select, answers.amount)
+    
+})
+
+
